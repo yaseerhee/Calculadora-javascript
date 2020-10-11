6a6bd5a86352ba727febdc2279395a8aa1df8a5f
Yaser Haddad <mohamedyaserhaddad@gmail.com>
2020-10-11 20:35:49 +0200

    first commit

---
+++ calc.js
@@ -0,0 +1,91 @@
+
+const botonNumeros= document.getElementsByName("data-number");
+const botonOpera = document.getElementsByName("data-opera");
+const botonIgual = document.getElementsByName("data-igual")[0];
+const botonDelete = document.getElementsByName("data-delete")[0];
+var result = document.getElementById("result");
+
+var opeActual = "";
+var opeAnterior = "";
+var operacion =  undefined;
+
+botonNumeros.forEach(function(boton){
+    boton.addEventListener("click", function(){
+        agregarNumero(boton.innerText);
+    })
+});
+
+botonOpera.forEach(function(boton){
+    boton.addEventListener("click", function(){
+        selectOperacion(boton.innerText);
+
+    })
+});
+
+botonIgual.addEventListener("click", function(){
+    calcular();
+    actualizarDisplay();
+
+});
+
+botonDelete.addEventListener("click", function(){
+    clear();
+    actualizarDisplay();
+});
+
+function agregarNumero(num){
+    opeActual = opeActual.toString() + num.toString();
+    actualizarDisplay();
+
+}
+
+function actualizarDisplay(){
+    result.value = opeActual;
+}
+
+function clear(){
+    opeActual = "";
+    opeAnterior = "";
+    operacion = undefined;
+
+}
+
+function selectOperacion(op){
+    if(opeActual == "") return ;
+    if(opeAnterior !== ""){
+        calcular();
+    }
+    operacion= op.toString();
+    opeAnterior = opeActual;
+    opeActual = "";
+} 
+
+function calcular(){
+    var calculo;
+    const anterior = parseFloat(opeAnterior);
+    const actual = parseFloat(opeActual);
+    if(isNaN(anterior) || isNaN(actual)) return;
+    switch(operacion){
+        case "+":
+            calculo = anterior + actual;
+            break;
+        case "-":
+            calculo = anterior - actual;
+            break;
+        case "*":
+            calculo = anterior * actual;
+            break;
+        case "/":
+            calculo = anterior / actual;
+            break;
+        default: 
+            return "Error";
+    }
+    opeActual = calculo;
+    operacion = undefined;
+    opeAnterior = "";
+}
+
+
+
+clear();
